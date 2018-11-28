5a0bb9195ae0015fb4442df4b0ab975ae04f3778
jeffb4real <urieow@gmail.com>
2018-11-27 23:40:03 -0800

    Regex tester complete

--- regex_tester.py
+++ regex_tester.py
@@ -2,7 +2,9 @@ import re
 
 pattern = r'\D(\d\d\d\d)\D'
 pattern = r'^\D*(\d\d\d\d)*\D*$'
-pattern = r'\D*(\d\d\d\d)\D*'
+pattern = r'^|\D(\d\d\d\d)\D|$'
+pattern = r'\D*(\d\d\d\d)\D*'    # <-- works 2nd best for finding year(s)
+pattern = r'\b(\d\d\d\d)\b'      # <-- works best for finding year(s)
 
 print ("Hi. I'm looking for this pattern: %s" % pattern)
 
